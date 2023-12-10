bc0cd79abe575ab52b96b52f0def99ad4cacde41
ansarianass <153439865+ansarianass@users.noreply.github.com>
2023-12-10 19:13:11 +0000

    variable update

---
+++ practice_01/01_variable.js
@@ -0,0 +1,17 @@
+const { log, Console } = require("console")
+
+const AccountId=1445588
+let AccountEmail="ansarianas@gmail.com"
+var AccountPassword="123457"
+AccountCity= "bhiwandi"
+// let see how it store and why it is used 
+AccountId=23
+
+
+// Console.log(AccountId)
+//  it show error becouse we cant change const type variable after declaration
+
+
+// we dont use var type variable becouse it is global scope variable and it expose itself in windows (on browser)
+
+// other variable like const , let is use becouse it is blockscope variable 

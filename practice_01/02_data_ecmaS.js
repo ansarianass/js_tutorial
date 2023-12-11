89b9c223c136f6109cde35e9f2de55fea54ddefa
ansarianass <153439865+ansarianass@users.noreply.github.com>
2023-12-11 15:17:51 +0000

    data_type reached

---
+++ practice_01/02_data_ecmaS.js
@@ -0,0 +1,26 @@
+"use strict"; // create all js code as newer version 
+console.log("primitive Data types ")
+//alert(3+3) //alert work in browser not in node 
+//code readability is periority first
+
+// datatype 
+// NNSSBU
+
+/*in datatypes null occupies its own place
+in case of temp if value is not mentioned we often use 0, 
+but in case of temperature 0c is value , hence null is used 
+
+*/ 
+/*int and bigint both value store number 
+number datatype value can store 2 to power 53 
+obbove this value bigint is use
+ */
+let AccountId=null;
+console.log(AccountId)
+console.log(typeof(null))
+/* type of used to obtain datatype of an variable 
+but whn you want to know type of null it show object 
+its imp to know this flex */ 
+console.log("object in js ")
+
+

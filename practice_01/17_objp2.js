c53884d310c9f598b88a780616432f14dfc4502c
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2023-12-28 14:35:08 +0000

    obj2

--- practice_01/17_objp2.js
+++ practice_01/17_objp2.js
@@ -20,3 +20,37 @@ const regularuser={
     age:18
 }
 console.log(regularuser);
+console.log(regularuser.fullname.firstname);
+console.log(regularuser.fullname.lastname);
+
+//optional chaining in object: 
+/*in most case when value come from backend and you dont confirm that 
+either value exist or not , in this case we use optional chaining method(?)
+
+
+*/
+/***************************************************************/ 
+//merging object into object
+let obj1={1:"a",2:"b",3:"c"}
+let obj2={24:"x",25:"y",26:"z"}
+
+
+
+//const obj3={obj1,obj2}
+//const obj3= Object.assign(obj1,obj2)
+//console.log(obj3)
+
+//{ '1': 'a', '2': 'b', '3': 'c', '24': 'x', '25': 'y', '26': 'z' }
+
+//in assign method we use two parameters(object.assign(Target,Source))
+//at target place we can hold empty object , on place of Source we have to mention object which we gonna attatch.
+const obj3=Object.assign({},obj1,obj2)
+console.log(obj3);
+
+
+/*********************************************************/
+//often we use spread operator instead of assign 
+const stor={...obj1,...obj2}
+console.log(stor);
+
+

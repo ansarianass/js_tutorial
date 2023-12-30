5b9e8993c2d1041c028f78a79083b010031b6161
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2023-12-30 10:30:37 +0000

    objp2

--- practice_01/17_objp2.js
+++ practice_01/17_objp2.js
@@ -23,6 +23,15 @@ console.log(regularuser);
 console.log(regularuser.fullname.firstname);
 console.log(regularuser.fullname.lastname);
 
+//we can access all keys and values of an object via some of comands
+console.log("keys of regularuser is :",Object.keys(regularuser));
+//obviously we can access all values of an object 
+console.log("Values of regularuser is",Object.values(regularuser));
+// it returns value as an array
+
+
+
+
 //optional chaining in object: 
 /*in most case when value come from backend and you dont confirm that 
 either value exist or not , in this case we use optional chaining method(?)
@@ -52,5 +61,32 @@ console.log(obj3);
 //often we use spread operator instead of assign 
 const stor={...obj1,...obj2}
 console.log(stor);
+
+/*********************************************************/
+console.log("***************************************************");
+const user=[
+    {
+        id:1,
+        email:"anas@google.com",
+
+    },
+    {
+        name:"ansari anas mmohd ejaz",
+        address:"Bhwandi, Thane , Maharastra"
+    }
+]
+console.log(user[0]);
+console.log(user[0].email);
+console.log(user[1]);
+console.log(user[1].address);
+
+//if i want to know either some property exist or not then we can simply tells to compiler 
+console.log(user.hasOwnProperty([].email));
+console.log(regularuser.hasOwnProperty('fullname'));
+
+
+
+
+
 
 

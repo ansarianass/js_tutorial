dc790813a7e835ab78efd12bc8276410b5589245
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2024-01-05 14:32:53 +0000

    function final

--- practice_01/20_function_ArrayObject.js
+++ practice_01/20_function_ArrayObject.js
@@ -23,7 +23,47 @@ console.log(ShoppingCart(20,23,34,55));
 
 // Case_02
 //if two more parameter add to function and provide the value what will be output
-console.log(ShoppingCart(20,23,34,55,34,45));//20,23 pass in val1,val2 ,remainig vslue comes with rest operator
+console.log(ShoppingCart(20,23,34,55,34,45));//20,23 pass in val1,val2 ,remainig value comes with rest operator
+
+
+
+
+//object in function 
+const user={
+    username:"Anas",
+    prize: 177,
+
+}
+//let see how to insert the object inside the function 
+function HandleObject(anyObject){
+console.log(`usrename is: ${anyObject.username} and price is: ${anyObject.prize }`)
+}
+//our function doesnt know what value hasbeen called and come , it worked based on parameter ANYOBJECT.
+HandleObject(user)
+
+// casae_01
+// if we wrote wrong startement by calling a object keys we got error 
+//i change prize to prices anyobject.prices
+// it return undefined at place of prize 
+
+// Case_02
+//we can create object instead of calling it 
+HandleObject({
+    username:"tauseef",
+    prize:200
+})
+
+// case_03
+const funArray=[2000,5000,8000]
+function Array(getArray){
+return getArray[1]
+}
+console.log(Array(funArray));//5000
+
+//Case_04
+//here we also can  pass value direct instead of calling 
+console.log(Array([20,50,80]));//50
+
 
 
 

a5b238a4779448581410601942d4afa504f6aa07
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2024-01-07 05:08:23 +0000

    arrow

--- practice_01/23_Arrow_This.js
+++ practice_01/23_Arrow_This.js
@@ -9,10 +9,27 @@ const user={
     prize:100,
     WelcomeMessage: function(){
 console.log(`${this.username}, Welcome to our website `);
+//console.log(this);// if we print only this inside the scope it give us current context from start where value got change outside and it return whole context change after 
     }
 }
+
+//console.log(this );// here after we consoile log in global scope thatswhy it return print statement as Anas and tauseef welcome message
+
+
 user.WelcomeMessage()//Anas, Welcome to our website
 user.username="tauseef"//current context chnage (value )
 user.WelcomeMessage()//Tauseef, welcome to our website 
+
+//what if we try to print this outside the function
+
+//console.log(this);//it return empty object becouse frome here no curent context in global scope. 
+
+console.log("*********************************************************");
+  function chai(){
+    let username="hitesh"
+    console.log(this.username);//undefined, this function work in object not in function 
+  }
+  chai()
+  console.log("*************************Arrow function********************************");
 
 

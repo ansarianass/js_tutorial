5f16b2c59b109518b40f2292f09fe59b07f1f1aa
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2024-01-15 16:11:33 +0000

    for loops in  js

---
+++ practice_01/26_Controlflow.js
@@ -0,0 +1,3 @@
+//terniary operators
+let iceprize=100
+iceprize>80?console.log("less Amount To Buy "):console.log("perfect amount to buy");
---
+++ practice_01/27_forloop.js
@@ -0,0 +1,15 @@
+//loops are also known as itrations 
+//for loops
+for (let index = 0; index < 10; index++) {
+    const element = index;
+   
+    console.log(element);
+    }
+//    
+for (let i = 0; i <12; i++) {
+    const element = i;
+    if(element==5){
+        console.log("5 is stop point Number");
+    }
+    
+}

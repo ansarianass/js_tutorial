54298384cf881339d0d74c0c7ad1658ee3fbfa0a
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2024-01-16 09:37:06 +0000

    do while loop

--- practice_01/28_While_do.js
+++ practice_01/28_While_do.js
@@ -1,8 +1,28 @@
 //while and do while loop
 /********************************************************************* */
 //while loop
-let index=0
-while (index<=10) 
+let i=0
+while (i<=10) 
 {
-    console.log();
+    console.log(`valus of index is ${i}`);
+    i=i+2
 }
+/*********************************************************************** */
+// while with Array
+MyArray=['tauseef Bhai','Ubaid','Kishor']
+index=0;
+while (index<MyArray.length) {
+    console.log(MyArray[index]);
+    index++;
+    
+}
+
+/********************************************************************** */
+// do-while loop 
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
+
+/************************************************************************ */

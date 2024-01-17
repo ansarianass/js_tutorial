54df81344048088090af3466d383f7444a5df7f1
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2024-01-17 12:53:01 +0000

    loops Special cases

--- practice_01/29_loop_Advanced.js
+++ practice_01/29_loop_Advanced.js
@@ -1,3 +1,14 @@
 /***********************************************************************/
 //Array specific loops  
 //1> for of loops
+//2>for in loops
+//3>for each loop
+//high order function
+/************************************************************************/
+//in most time we recieve Array with String inside of it 
+//['','','','',]
+// or even we have objects inside an array 
+//[{},{},{}]
+
+// lets see one by one each of this 
+

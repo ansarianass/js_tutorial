d1bd15b8052ea951b6c1da1678ac1408c2167c25
Anas Ansari <153439865+ansarianass@users.noreply.github.com>
2024-01-20 11:08:52 +0000

    for loops in advanced

--- practice_01/31_for_each.js
+++ practice_01/31_for_each.js
@@ -1,5 +1,15 @@
+//for each is known as higher order function 
 const lang=['c++','python','c','javascript']
+lang.forEach(function(code){
+console.log(code);
+})
+//each time code name function  will run and return value of an array untill it done 
 
-array.forEach(element => {
-    
-});
+/****************************************************************/
+//arrow function 
+
+ lang.forEach((element) => {
+    console.log(element);
+ });
+
+ /*****************************************************************/
